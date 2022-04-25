37409cf7136415fb4b3b68c8ad14c9acfef549c8
Chirag Murari <84890563+Chirag-Murari@users.noreply.github.com>
2022-04-25 12:46:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,7 @@
     <title>Restaurant</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Free Template by Free-Template.co" />
-    <meta name="keywords" content="free bootstrap 4, free bootstrap 4 template, free website templates, free html5, free template, free website template, html5, css3, mobile first, responsive" />
-    <meta name="author" content="Free-Template.co" />
+    
     
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700|Raleway" rel="stylesheet">
     <link rel="stylesheet" href="css/bootstrap.min.css">
